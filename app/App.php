3dcdb048fac03234954707a01e390745f7330d4c
Daniel Arrudas <arrudasmacedo@gmail.com>
2025-07-22 14:43:32 -0300

    Refactor CSV handling functions and improve HTML structure in transactions view

--- app/App.php
+++ app/App.php
@@ -1,21 +1,31 @@
 <?php
 
-declare(strict_types = 1);
-function format_csv(array $files) :array{
+declare(strict_types=1);
+function format_csv(array $files): array
+{
     $files = array_diff($files, [".", ".."]);
     $newKeys = [];
-    for($i = 0; $i < count($files); $i++){
-        array_push($newKeys,$i);
+    for ($i = 0; $i < count($files); $i++) {
+        array_push($newKeys, $i);
     }
     $files = array_combine($newKeys, $files);
     return $files;
 }
-function scan_directory(string $path): array{
+function scan_directory(string $path): array
+{
     $files = scandir($path);
     $files = format_csv($files);
     return $files;
 }
-function get_csvs_data(): array{
+function string_to_float($string): float
+{
+    $string = str_replace("$", "", $string);
+    $string = str_replace(",", "", $string);
+    $amount = floatval($string);
+    return $amount;
+}
+function get_csvs_data(): array
+{
     $csvs = scan_directory(FILES_PATH);
     $data = [];
     //Esse for passa por cada csv no transaction_files
@@ -26,13 +36,12 @@ function get_csvs_data(): array{
         if ($file) {
             //passa por cada linha do csv e guarda no array data sem o header
             while (($line = fgetcsv($file)) !== false) {
-                if($j === 0){
+                if ($j === 0) {
                     $j++;
                     continue;
                 }
-                $line[3] = str_replace("$", "", $line[3]);
-                $amount = floatval($line[3]);
-                $data[] = ["Date" => $line[0], "Check #" => $line[1], "Description" => $line[2], "Amount" => $amount]; 
+                $amount = string_to_float($line[3]);
+                $data[] = ["Date" => $line[0], "Check #" => $line[1], "Description" => $line[2], "Amount" => $amount];
                 $j++;
             }
             fclose($file);
@@ -40,24 +49,26 @@ function get_csvs_data(): array{
             echo 'Error opening file';
         }
     }
-    
+
     return $data;
 }
-function print_csv() {
+function print_csv()
+{
     $data = get_csvs_data();
-    var_dump($data);
-    // foreach ($data as $line) {
-    //     echo "<tr>";
-    //     echo "<td>" . $line["Date"] . "</td>";
-    //     echo "<td>" . $line["Check #"] . "</td>";
-    //     echo "<td>" . $line["Description"] . "</td>";
-    //     echo "<td>" . $line["Amount"] . "</td>";
-    //     echo "</tr>";
-    // }
+    foreach ($data as $line) {
+        echo "<tr>";
+        echo "<td>" . $line["Date"] . "</td>";
+        echo "<td>" . $line["Check #"] . "</td>";
+        echo "<td>" . $line["Description"] . "</td>";
+        $amount = number_format($line["Amount"], 2, ".", ",");
+        if ($line["Amount"] < 0) {
+            $amount = substr_replace($amount, "$", 1, 0);
+            echo "<td class='expense'>" . $amount . "</td>";
+        }
+        if ($line["Amount"] > 0)
+            echo "<td class='income'>$" . $amount . "</td>";
+        echo "</tr>";
+    }
 }
-//TODO: consertar cast data type
-$texto = "-$1,303.97";
-$texto = str_replace("$", "", $texto);
-echo "{$texto} <br>";
-$amount = (float)$texto;
-echo "{$amount}";
+
+$value = 1564.21;
--- views/transactions.php
+++ views/transactions.php
@@ -1,59 +1,72 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>Transactions</title>
-        <style>
-            table {
-                width: 100%;
-                border-collapse: collapse;
-                text-align: center;
-            }
 
-            table tr th, table tr td {
-                padding: 5px;
-                border: 1px #eee solid;
-            }
+<head>
+    <title>Transactions</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            text-align: center;
+        }
 
-            tfoot tr th, tfoot tr td {
-                font-size: 20px;
-            }
+        table tr th,
+        table tr td {
+            padding: 5px;
+            border: 1px #eee solid;
+        }
 
-            tfoot tr th {
-                text-align: right;
-            }
-        </style>
-    </head>
-    <body>
-        <table>
-            <thead>
-                <tr>
-                    <th scope="col">Date</th>
-                    <th scope="col">Check #</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
+        tfoot tr th,
+        tfoot tr td {
+            font-size: 20px;
+        }
+
+        tfoot tr th {
+            text-align: right;
+        }
+
+        .income {
+            color: green;
+        }
+
+        .expense {
+            color: red;
+        }
+    </style>
+</head>
+
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th scope="col">Date</th>
+                <th scope="col">Check #</th>
+                <th scope="col">Description</th>
+                <th scope="col">Amount</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
                 <?php
                 print_csv();
                 ?>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <th colspan="3">Total Income:</th>
-                    <td><!-- YOUR CODE --></td>
-                </tr>
-                <tr>
-                    <th colspan="3">Total Expense:</th>
-                    <td><!-- YOUR CODE --></td>
-                </tr>
-                <tr>
-                    <th colspan="3">Net Total:</th>
-                    <td><!-- YOUR CODE --></td>
-                </tr>
-            </tfoot>
-        </table>
-    </body>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <th colspan="3">Total Income:</th>
+                <td><!-- YOUR CODE --></td>
+            </tr>
+            <tr>
+                <th colspan="3">Total Expense:</th>
+                <td><!-- YOUR CODE --></td>
+            </tr>
+            <tr>
+                <th colspan="3">Net Total:</th>
+                <td><!-- YOUR CODE --></td>
+            </tr>
+        </tfoot>
+    </table>
+</body>
+
 </html>
